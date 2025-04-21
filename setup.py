c4c1ea62072f132c50adcf3827f266dbdd20784a
John Mike <jmasamvu@gmail.com>
2025-04-21 15:05:31 +0200

    added setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,23 @@
-from setuptools import find_packages,setup
+from setuptools import find_packages, setup
 from typing import List
 
+HYPHEN_E_DOT = '-e .'
 
-HYPHEN_E_DOT=' -e .'
-def get_requirements(filepath:str)->list:
+def get_requirements(filepath: str) -> List[str]:
     '''This function reads the requirements file 
     and returns a list of requirements'''
-    requirements = []
     with open(filepath) as file_obj:
         requirements = file_obj.readlines()
-        requirements = [req.replace('\n','') for req in requirements]
-
+        requirements = [req.strip() for req in requirements]  # .strip() is neater than replace
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-    
     return requirements
 
 setup(
-name = 'cellvoltage',
-version = '0.0.1',
-author='John Mike',
-author_email='jmasamvu@gmail.com',
-packages=find_packages(),
-install_requires=get_requirements('requirements.txt'),
-
-
-
-
-
+    name='cellvoltage',
+    version='0.0.1',
+    author='John Mike',
+    author_email='jmasamvu@gmail.com',
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt'),
 )
